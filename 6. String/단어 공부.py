ff05a8ca20110390ce2cd819add679813347c274
Hun seok SUN <xodidxkq@naver.com>
2023-01-07 13:07:14 +0900

    add file

---
+++ 6. String/단어 공부.py
@@ -0,0 +1,15 @@
+words =  input().upper()
+new_words = list(set(words))
+
+cnt_list = []
+
+for i in new_words:
+    cnt = words.count(i)
+    cnt_list.append(cnt)
+
+if cnt_list.count(max(cnt_list)) > 1 :
+    print('?')
+
+else :
+    max_index = cnt_list.index(max(cnt_list))  
+    print(new_words[max_index])
