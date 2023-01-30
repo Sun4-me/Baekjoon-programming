c40e97bc959c0ec293f846f07985630ed4fbad61
Hun seok SUN <xodidxkq@naver.com>
2023-01-30 17:31:10 +0900

    add file

---
+++ Bronze/10808 알파벳 개수.py
@@ -0,0 +1,5 @@
+a = list(str(input()))
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+for i in range(len(alphabet)):
+    b = a.count(alphabet[i])
+    print(b, end=' ')
