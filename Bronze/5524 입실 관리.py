640f478622f0190352dfb96c8678c999ab388674
Hun seok SUN <xodidxkq@naver.com>
2023-02-11 20:49:19 +0900

    add file

---
+++ Bronze/5524 입실 관리.py
@@ -0,0 +1,5 @@
+N = int(input())
+
+for _ in range(N):
+    low = input().lower()
+    print(low)
---
+++ Bronze/9316 Hello Judge.py
@@ -0,0 +1,5 @@
+N = int(input())
+
+for i in range(1,N+1):
+    tmp = "Hello World, Judge " + str(i) + "!"
+    print(tmp)
