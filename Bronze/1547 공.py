70a8e5eec5c1bc0457b82a5ae914008422d911ff
Hun seok SUN <xodidxkq@naver.com>
2023-03-26 15:33:49 +0900

    add file

---
+++ Bronze/1547 공.py
@@ -0,0 +1,12 @@
+M = int(input())
+
+cups = [1,2,3]
+for _ in range(M):
+    x, y = map(int, input().split())
+    
+    xi = cups.index(x)
+    yi = cups.index(y)
+    
+    cups[xi], cups[yi] = cups[yi], cups[xi]
+    
+print(cups[0])
