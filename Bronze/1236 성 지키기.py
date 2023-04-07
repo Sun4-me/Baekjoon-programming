83a371183705f619a7c472dc7c8b523c899f0fdf
Hun seok SUN <xodidxkq@naver.com>
2023-04-07 23:17:17 +0900

    add file

---
+++ Bronze/1236 성 지키기.py
@@ -0,0 +1,17 @@
+n, m = map(int,input().split())
+castle = []
+
+for _ in range(n):
+    castle.append(input())
+
+a, b = 0, 0
+
+for i in range(n):
+    if "X" not in castle[i]:
+        a += 1
+
+for j in range(m):
+    if "X" not in [castle[i][j] for i in range(n)]:
+        b += 1
+
+print(max(a ,b))
