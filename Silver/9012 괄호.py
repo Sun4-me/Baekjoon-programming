cc1dda939d352e74164c3837c442f4ada6205114
Hun seok SUN <xodidxkq@naver.com>
2023-04-10 13:35:52 +0900

    add file

---
+++ Silver/9012 괄호.py
@@ -0,0 +1,15 @@
+for _ in range(int(input())):
+    bracket = input()
+    sum = 0
+    for i in bracket:
+        if i == '(':
+            sum += 1
+        elif i == ')':
+            sum -= 1
+        if sum < 0:
+            print('NO')
+            break
+    if sum > 0:
+        print('NO')
+    elif sum == 0:
+        print('YES')
