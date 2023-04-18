9cfc65f6f0b7cc92fc8f633d6787f55a01db99f6
Hun seok SUN <xodidxkq@naver.com>
2023-04-19 00:23:23 +0900

    feat: Add stack implementation using list

---
+++ Algorithm Theory/DFS & BFS/01. 스택.py
@@ -0,0 +1,17 @@
+# 먼저 들어 온 데이터가 나중에 나가는 형식(선입후출)의 자료구조
+# 입구와 출구가 동일한 형태
+
+stack = []
+
+# 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제()
+stack.append(5)
+stack.append(2)
+stack.append(3)
+stack.append(7)
+stack.pop()
+stack.append(1)
+stack.append(4)
+stack.pop()
+
+print(stack)  # 최하단 원소부터 출력
+print(stack[::-1])  # 최상단 원소부터 출력
