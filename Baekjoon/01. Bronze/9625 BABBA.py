bccbc01b03c7d81db6e2380b380e9c3f3a4aaee3
Hun seok SUN <xodidxkq@naver.com>
2023-08-20 23:09:27 +0900

    feat: Add solution for BOJ problem 9625

---
+++ Baekjoon/01. Bronze/9625 BABBA.py
@@ -0,0 +1,15 @@
+k = int(input())
+n = 46
+
+a = [0] * 46
+a[0] = 1
+a[1] = 0
+
+b = [0] * 46
+b[0] = 0
+b[1] = 1
+
+for i in range(2, n):
+    a[i] = a[i - 1] + a[i - 2]
+    b[i] = b[i - 1] + b[i - 2]
+print(a[k], b[k])
