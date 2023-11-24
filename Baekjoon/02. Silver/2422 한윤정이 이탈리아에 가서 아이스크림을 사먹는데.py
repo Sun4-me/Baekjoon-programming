c6b6d59b0ff1edbaf12436c6c4ba76d8ee25f49f
Hun seok SUN <xodidxkq@naver.com>
2023-11-24 15:33:29 +0900

    feat: Add solution for BOJ problem 2422

---
+++ Baekjoon/02. Silver/2422 한윤정이 이탈리아에 가서 아이스크림을 사먹는데.py
@@ -0,0 +1,22 @@
+import sys
+from itertools import combinations
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+ice = [[False for _ in range(N)] for _ in range(N)]
+
+for i in range(M):
+    a, b = map(int, input().split())
+    ice[a - 1][b - 1] = True
+    ice[b - 1][a - 1] = True
+
+result = 0
+
+for i in combinations(range(N), 3):
+    if ice[i[0]][i[1]] or ice[i[0]][i[2]] or ice[i[1]][i[2]]:
+        continue
+    result += 1
+
+print(result)
