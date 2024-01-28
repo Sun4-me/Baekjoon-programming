5e5db58555a9af7f79ec88734de4c950405f986a
Hun seok SUN <xodidxkq@naver.com>
2024-01-28 23:52:52 +0900

    feat: Add solution for BOJ problem 18512

---
+++ Baekjoon/01. Bronze/18512 점프 점프.py
@@ -0,0 +1,16 @@
+x, y, p1, p2 = map(int, input().split())
+cnt = 0
+while 1:
+    if p1 == p2:
+        break
+    if cnt > 1000:
+        p1 = -1
+        break
+    if p1 > p2:
+        p2 += y
+
+    elif p1 < p2:
+        p1 += x
+    cnt += 1
+
+print(p1)
